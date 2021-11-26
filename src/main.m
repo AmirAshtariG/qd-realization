194b0074f615c057d08f1bf09b30a023a0724c03
Amir <amirashtari.1374@gmail.com>
2021-11-26 16:42:18 +0100

    UPDATE Scenario

--- src/main.m
+++ src/main.m
@@ -54,7 +54,7 @@ end
 
 %% Input
 % Leave empty for default 'ScenarioTest'
-scenarioNameStr = 'examples/StreetCanyonRIS';
+scenarioNameStr = 'examples/StreetCanyonRISGIF';
 
 if ~isempty(scenarioNameStr)
     fprintf('Use customized scenario: %s.\n',scenarioNameStr);
